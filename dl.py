eac829d8e238007e5c93c6de752e7d35d8611b13
yuanchima <yuanchima@yeah.net>
2021-01-05 11:00:00 +0800

    add user_name and password input field

--- dl.py
+++ dl.py
@@ -2,10 +2,11 @@ import time
 import os
 import concurrent.futures
 
-def download(url):
-    cmd = r'wget -N --load-cookies C:\Users\yuanchima\Downloads\Documents\.urs_cookies --save-cookies C:\Users\yuanchima\Downloads\Documents\.urs_cookies --auth-no-challenge=on --keep-session-cookies --user=yuanchima --password=earthdata_M16384 --content-disposition -i'
-    cmd = cmd + ' ' + url
-    os.system(cmd)
+def download(user_name, password):
+    def dl(input_file):
+        cmd = f'wget -N -nv --load-cookies .urs_cookies --save-cookies .urs_cookies --auth-no-challenge=on --keep-session-cookies --user={user_name} --password={password} --content-disposition -i {input_file}'
+        os.system(cmd)
+    return dl
 
 def load_url(file_path):
     with open(file_path, 'r') as f:
@@ -18,7 +19,6 @@ def split_file(all_url, file_nums, subset_root='./subset'):
         files = os.listdir(subset_root)
         for f in files:
             os.remove(f"{subset_root}/{f}")
-            # print(f'remove {f}')
     else:
         os.mkdir(subset_root)
 
@@ -28,29 +28,31 @@ def split_file(all_url, file_nums, subset_root='./subset'):
         with open(f'{subset_root}/subset_{i}.txt', 'w') as f:
             if i != rows_per_file-1:
                 f.writelines([f'{url}\n' for url in all_url[i*rows_per_file: (i+1)*rows_per_file]])
-                # print(f'create {subset_root}/subset_{i}.txt')
             else:
                 f.writelines([f'{url}\n' for url in all_url[i*rows_per_file:]])
-                # print(f'create {subset_root}/subset_{i}.txt')
         
-
-def threadPoolExecutor(file_list, num_worker=5):
+def threadPoolExecutor(file_list, downloader, num_worker=5):
     with concurrent.futures.ThreadPoolExecutor(num_worker) as executor:
-        results = [executor.submit(download, f) for f in file_list]
+        results = [executor.submit(downloader, f) for f in file_list]
         for f in concurrent.futures.as_completed(results):
             print(f.result())
 
 
 if __name__ == "__main__":
+    user_name = input("user name: ")
+    password = input("password: ")
+    downloader = download(user_name, password)
+
     start = time.perf_counter()
+
     file_path = 'subset_M2T1NXSLV_5.12.4_20210104_062202.txt'
     all_url = load_url(file_path)
     subset_root = './subset'
     split_file(all_url, 100, subset_root)
     f_lst= [f'{subset_root}/{f}' for f in os.listdir(subset_root)]
-    # print(f_lst)
-    # threadPoolExecutor(f_lst, num_worker=5)
-    download(f_lst[0])
+
+    threadPoolExecutor(f_lst, downloader, num_worker=50)
+    # download(f_lst[0])
 
     finish = time.perf_counter()
     print(f'Finished in {round(finish-start, 2)} second(s)')
